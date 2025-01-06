127e8b634389b1f3489c7341369cfb18875d041e
Kevin Esaa <kevin.esaa@gmail.com>
2025-01-06 15:56:17 -0400

    feat: add status codes CommonStatusCodes.py

--- statusCodeResponses/CommonStatusCodes.py
+++ statusCodeResponses/CommonStatusCodes.py
@@ -32,6 +32,8 @@ class CommonStatusCodes(ABC):
     def __start(statusCodesBuilder:StatusCodesBuilder):
         if(not CommonStatusCodes.__common_sync):
             CommonStatusCodes.OK = statusCodesBuilder.generateStatusModel(200,"OK","ok")
+            CommonStatusCodes.BODY_NOT_FOUND = statusCodesBuilder.generateStatusModel(400,"BODY_NOT_FOUND","the request body is required")
+            CommonStatusCodes.FAIL_PARSING_JSON_BODY = statusCodesBuilder.generateStatusModel(400,"FAIL_PARSING_JSON_BODY","not valid format body")
             CommonStatusCodes.INTERNAL_SERVER_ERROR = statusCodesBuilder.generateStatusModel(500,"INTERNAL_SERVER_ERROR","ups! this should be not happening")
             CommonStatusCodes.UNKNOWN_ERROR = statusCodesBuilder.generateStatusModel(500,"UNKNOWN_ERROR","ups!")
             
@@ -39,5 +41,7 @@ class CommonStatusCodes(ABC):
     
     # can be use in all my life
     OK:StatusCodeModel
+    BODY_NOT_FOUND:StatusCodeModel
+    FAIL_PARSING_JSON_BODY:StatusCodeModel
     INTERNAL_SERVER_ERROR:StatusCodeModel
     UNKNOWN_ERROR:StatusCodeModel
